d4b1f4ff0aaca4b280efe465e90544050f3de752
Ronit Chattopadhyay <ronit.chattopadhyay@quikr.com>
2020-03-29 21:22:56 +0530

    Init commit

---
+++ src/main/java/com/vinava/pofo/PofoApplication.java
@@ -0,0 +1,13 @@
+package com.vinava.pofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PofoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinava/pofo/PofoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinava.pofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
