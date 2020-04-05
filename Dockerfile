7dc6c46cec8216e5c3ca35d68141e15ed2541a2e
ronit0717 <ronit0717@gmail.com>
2020-04-05 19:22:08 +0530

    docker changes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
 FROM openjdk:8
-ADD target/pofo-app.jar pofo-app.jar
-EXPOSE 5555
+
+ARG profile=dev
+
+COPY ./src /opt/code/pofo/src
+COPY ./pom.xml  /opt/code/pofo/pom.xml
+COPY ./startup.sh /opt/code/pofo/startup.sh
+WORKDIR /opt/code/pofo
+
+RUN mvn clean install -P$profile
+RUN rm -rf src
+
+CMD ["bash","startup.sh",$profile]
--- startup.sh
+++ startup.sh
@@ -1,14 +1,2 @@
-echo "Startup pofo-app"
-if [ -z $1 ]
-then
-  echo "Profile detail not present... startup failed"
-  exit 1
-fi
-
-echo "Initiating build... profile : "$1
-mvn clean install -P$1 || exit 1
-echo "Creating docker image"
-docker build . -t pofo-app || exit 1
-echo "Launcing application pofo-app"
-java -jar -Dspring.profiles.active="$1" target/pofo-app.jar || exit 1
-exit 0
+echo "Launcing application pofo-api"
+java -jar -Dspring.profiles.active="$1" target/pofo-api.jar || echo "Server Start Failed";exit 1
