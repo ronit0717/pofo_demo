c97fa2638863fec55301198b291a776f25328ff3
root <rohit.chattopadhyay1@gmail.com>
2020-04-07 21:02:40 +0800

    Change container name

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "<<< Deleting existing container pofo-api>>>"
-docker rm -f pofo-api
+docker rm -f pofo-stage-api
 echo "<<< Building application pofo api for environment : $1 >>>" 
 mvn clean install -DskipTests -P"$1" || exit 1
 echo "<<< Maven build successful.. doing docker build >>>"
 docker build --build-arg profile="$1" . -t pofo-api || exit 1
 echo "<<< Docker image created successfully >>>"
 docker "<<< Build process completed successfully >>>"
+
