93682833a3db022c66e8222bcfc4b8cd9e2f6c0b
Ronit Chattopadhyay <ronit0717@gmail.com>
2020-04-07 20:37:34 +0530

    Code added to remove existing image

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "<<< Deleting existing container pofo-api>>>"
 docker rm -f pofo-stage-api
+docker rmi pofo-api
 echo "<<< Doing docker build with profile: $1 >>>"
 docker build --build-arg profile="$1" . -t pofo-api || exit 1
 echo "<<< Docker image created successfully.. run the docker now >>>"
