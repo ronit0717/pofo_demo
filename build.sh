ae8bf32113f85516fe1d0975f90915f434e39bfc
Ronit Chattopadhyay <ronit0717@gmail.com>
2020-04-07 16:56:56 +0530

    docker run command removed

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ echo "<<< Building application pofo api for environment : $1 >>>"
 mvn clean install -DskipTests -P"$1" || exit 1
 echo "<<< Maven build successful.. doing docker build >>>"
 docker build --build-arg profile="$1" . -t pofo-api || exit 1
-echo "<<< Docker image created successfully, creating docker container >>>"
-docker run -p 5555:5555 --name pofo-api -d pofo-api "$1" || exit 1
+echo "<<< Docker image created successfully >>>"
 docker "<<< Build process completed successfully >>>"
