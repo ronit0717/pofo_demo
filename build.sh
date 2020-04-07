a2d0979a424f50fd9dd02a1de452874dd6bb5e6d
Ronit Chattopadhyay <ronit0717@gmail.com>
2020-04-07 16:33:33 +0530

    skipped unit tests in uat

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 echo "<<< Deleting existing container pofo-api>>>"
 docker rm -f pofo-api
 echo "<<< Building application pofo api for environment : $1 >>>" 
-mvn clean install -P"$1" || exit 1
+mvn clean install -DskipTests -P"$1" || exit 1
 echo "<<< Maven build successful.. doing docker build >>>"
 docker build --build-arg profile="$1" . -t pofo-api || exit 1
 echo "<<< Docker image created successfully, creating docker container >>>"
-docker run -p 5555:5555 --name pofo-api -d pofo-api || exit 1
+docker run -p 5555:5555 --name pofo-api -d pofo-api "$1" || exit 1
 docker "<<< Build process completed successfully >>>"
