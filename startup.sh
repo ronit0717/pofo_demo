627a03ba7d4a227e2bf090f62484f2d45b7901a2
root <rohit.chattopadhyay1@gmail.com>
2020-04-08 00:21:00 +0800

    Docker Container Startup Bug fix

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 echo "Building the application with environment : $1"
 mvn clean install -P"$1"
 echo "Launcing application pofo-api for environment : $1"
-java -jar -Dspring.profiles.active="$1" /opt/code/target/pofo-api.jar || echo "Pofo Api Server Start Failed";exit 1
+java -jar -Dspring.profiles.active="$1" /target/pofo-api.jar || echo "Pofo Api Server Start Failed";exit 1
