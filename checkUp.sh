f9d40b4b2c74fc933253ddefec6450309d771122
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2020-04-07 20:01:04 +0000

    Update checkUp.sh

--- checkUp.sh
+++ checkUp.sh
@@ -2,12 +2,11 @@ ip="$(docker inspect pofo-stage-api | grep IPAddress | tail -1 | grep -oE '\b([0
 counter=0
 while ! curl -sf http://$ip:5555/pofo/client/test; do
   sleep 1
-  echo "Server not yet started, waiting"
+  docker logs -t --tail 1 pofo-stage-api
   counter=$((counter+1))
-  if [[ "$counter" -gt 300 ]]; then
-       echo "Waited for 5 minutes; terminating Server"
+  if [[ "$counter" -gt 420 ]]; then
+       echo "Waited for 7 minutes; terminating Server"
        docker rm -f pofo-stage-api
-       docker logs pofo-stage-api
        exit 1
   fi
 done
