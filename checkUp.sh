e23522053ef27af7708e00d1c7b00b0684447bc5
Rohit Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2020-04-08 10:24:38 +0530

    Correct Docker Container name

--- checkUp.sh
+++ checkUp.sh
@@ -2,13 +2,13 @@ ip="$(docker inspect pofo-stage-api-new | grep IPAddress | tail -1 | grep -oE '\
 counter=0
 while ! curl -sf http://$ip:5555/pofo/client/test; do
   sleep 1
-  docker logs -t --tail 1 pofo-stage-api
+  docker logs -t --tail 1 pofo-stage-api-new
   counter=$((counter+1))
   if [[ "$counter" -gt 420 ]]; then
-       echo "Waited for 7 minutes; terminating Server"
+       echo "Waited for 7 minutes; terminating Container"
        docker rm -f pofo-stage-api-new
        exit 1
   fi
 done
 echo
-echo "Server Started"
+echo "Server started in container"
