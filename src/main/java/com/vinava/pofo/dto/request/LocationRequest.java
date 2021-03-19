64da06229c802ff1eb686f375f404fa02dfa33d3
Ronit Chattopadhyay <ronit0717@gmail.com>
2021-03-19 18:25:12 +0530

    Cleaned location request

--- src/main/java/com/vinava/pofo/dto/request/LocationRequest.java
+++ src/main/java/com/vinava/pofo/dto/request/LocationRequest.java
@@ -1,14 +1,12 @@
 package com.vinava.pofo.dto.request;
 
 import com.vinava.pofo.model.Location;
-import com.vinava.pofo.model.embed.ProductAttribute;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Set;
 
 @Data
 @Builder
@@ -23,8 +21,6 @@ public class LocationRequest {
     private String pincode;
 
     private String description;
-
-    private Set<ProductAttribute> productAttributes;
 
     public Location from(long clientId) {
         return Location.builder()
